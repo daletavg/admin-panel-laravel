de1377aed492a9949b93d60b2cc935dc7651e487
Vadim Dubrovskiy <programmer@Mac-mini-Programmer.local>
2019-11-21 16:25:01 +0200

    Add deprecated method at HasLangDataContract

--- app/Contracts/HasLangData.php
+++ app/Contracts/HasLangData.php
@@ -7,6 +7,12 @@ namespace App\Contracts;
 interface HasLangData
 {
     function lang($locale = null);
+
+    /**
+     * @deprecated
+     * @param string $className
+     * @return mixed
+     */
     function setLangClass(string $className) ;
     function getLangClass():string ;
 }
